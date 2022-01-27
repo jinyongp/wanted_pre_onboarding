1ff4ed2b095d5d5bbaf82d2f661a5c56b041606a
jinyongp <dev.jinyongp@gmail.com>
2022-01-27 14:48:11 +0900

    Setup project

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as AutoComplete } from './AutoComplete';
+export { default as ClickToEdit } from './ClickToEdit';
+export { default as Modal } from './Modal';
+export { default as Tab } from './Tab';
+export { default as Tag } from './Tag';
+export { default as Toggle } from './Toggle';
